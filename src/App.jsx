9dc283e9f026bc0c24afdcaa4fd394fa64ee2d16
fischer8 <fischxr@hotmail.com>
2022-08-24 19:12:14 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import ZapButton from './Components/ZapButton';
 import Header from './Components/Header';
 import Main from './Components/Main';
 import DialogBox from './Components/DialogBox';
+import Footer from './Components/Footer';
+import Copyright from './Components/Copyright';
 import './App.css';
+import logo_image from './images/elementos/lavita_logotipo branco.png';
+import header_image from './images/elementos/lavita_barquinhos_semreboco.png';
 
 class App extends React.Component {
   render() {
     return (
       <section>
-        <Header />
+        <ZapButton />
+        <Header cover={header_image} logo={logo_image} />
         <Main />
         <DialogBox />
+        <Footer logo={logo_image} />
+        <Copyright />
       </section>
     );
   }
