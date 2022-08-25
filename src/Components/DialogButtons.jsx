b76e9c43402772bc0f3d3a069bf55528368f2e87
fischer8 <fischxr@hotmail.com>
2022-08-25 17:05:47 -0300

    update

--- src/Components/DialogButtons.jsx
+++ src/Components/DialogButtons.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 class DialogButtons extends React.Component {
   render () {
     const zapZap = 'https://api.whatsapp.com/send?phone=5511994352770';
-    const location = 'https://api.whatsapp.com/send?phone=5511994352770';
+    const location = 'https://goo.gl/maps/BBaptvShMhKwvunb6';
     return (
       <section className="dialog-buttons">
-        <a className="contact-button" href={zapZap}>Contato</a>
-        <a className="location-button" href={location}>Localização no Maps</a>
+        <a className="contact-button" href={zapZap} rel="noreferrer" target="_blank">Contato</a>
+        <a className="location-button" href={location} rel="noreferrer" target="_blank">Localização no Maps</a>
       </section>
     );
   }
--- src/Components/FooterArticle.jsx
+++ src/Components/FooterArticle.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 class FooterArticle extends React.Component {
   render () {
-    const zapZap = 'https://api.whatsapp.com/send?phone=5511994352770';
+    const booking = 'https://www.booking.com/Share-UvH4byv';
     return (
       <article className="footer-article-container">
-          <h5 className="footer-address">São Miguel dos Milagres - AL - Brasil</h5>
-          <a href={zapZap} rel="noreferrer" target="_blank">
+          <h5 className="footer-address">Tv. Francisco de Lima, São Miguel dos Milagres - AL, 57940-000</h5>
+          <a href={booking} rel="noreferrer" target="_blank">
             <span className="footer-button">Faça sua reserva</span>
           </a>
       </article>
