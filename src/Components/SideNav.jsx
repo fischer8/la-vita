899d0b04ff75647fa2d7f051929b50f3b7098eac
fischer8 <fischxr@hotmail.com>
2022-08-26 09:23:22 -0300

    styles

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 class SideNav extends React.Component {
   render () {
     return (
-      <nav className="SideNav">
-        <li>Fotos</li>
-        <li>Suites</li>
-        <li>Contato</li>
+      <nav className="side-nav">
+        <span className="close-nav-button">X</span>
+        <ul>
+          <li className="side-nav-opts">Fotos</li><hr />
+          <li className="side-nav-opts">Suites</li><hr />
+          <li className="side-nav-opts">Contato</li>
+        </ul>
       </nav>      
     );
   }
