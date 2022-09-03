be804f6444f3ea00f673e52326a22faf6149257c
fischer8 <fischxr@hotmail.com>
2022-09-03 15:53:58 -0300

    footer nav upt

--- src/Components/MainComponents/FooterNav.jsx
+++ src/Components/MainComponents/FooterNav.jsx
@@ -1,15 +1,13 @@
 import { Component } from 'react';
+import Nav from './Nav';
 
 export default class FooterNav extends Component {
   render() {
-    const navOpts = ['Home','Suítes','Fotos'];
-    const options = navOpts.map((opt) => <span className='footer-nav-opts' key={opt}>{opt}</span>)
+    const opts = ['Home','Suítes','Fotos'];
     return (
       <section className='footer-nav-container'>
         <h2 className='footer-nav-title'>Explore</h2>
-        <nav className='footer-nav'>
-          {options}
-        </nav>
+        <Nav opts={opts} navclname='footer-nav' optclname='footer-nav-opts'/>
       </section>
     );
   }
